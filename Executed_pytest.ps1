17dbffa75e1ac198a9c7c3912670873d25cdd383
Huang <ziru.huang@aspentech.com>
2022-02-18 15:52:00 +0800

    update for MVT

--- Executed_pytest.ps1
+++ Executed_pytest.ps1
@@ -1,8 +1,8 @@
 $pythonroot = "C:\Python39"
 $py = Join-Path -Path $pythonroot -ChildPath "python.exe"
-$Project = "C:\p4\ApemMobile_UIautomation\DesktopApem"
-$testcase = Join-Path -Path $Project -ChildPath "testcase\AllTest.py"
-Set-Location -Path $pythonroot
+$Project = "C:\p4\ApemMobile_UIautomation\DesktopApem\testcase"
+$testcase = Join-Path -Path $Project -ChildPath "AllTest.py"
+Set-Location -Path $Project
 $process = Start-Process -FilePath $py -ArgumentList $testcase
 while($process.Responding)
 {
