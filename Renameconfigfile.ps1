7de04134723bc0e486c054c4761ec211843c724e
huang-ziru <ziru.huang@aspentech.com>
2023-09-01 11:58:50 +0800

    update0901

--- Renameconfigfile.ps1
+++ Renameconfigfile.ps1
@@ -4,14 +4,5 @@ $Project = "C:\p4\UIautomation"
 $testcase = Join-Path -Path $Project -ChildPath "pendingFile.py"
 $ResultFile = "C:\p4\UIautomation\log.xml"
 Set-Location -Path $Project
- Start-Process -FilePath $py -ArgumentList $testcase
- $process = Get-Process -Name "python"
-while($process.Responding)
-{
-Start-Sleep -Seconds 10
-  if(Test-Path -Path $ResultFile)
-  {
-  Stop-Process -Name "python"
-    break
-  }
- }
+Start-Process -FilePath $py -ArgumentList $testcase
+$process = Get-Process -Name "python"
